5c921e1f0f007a97fe0b7ac23d2364c4587d9501
WINSDK <winsdk@vmi356273.contaboserver.net>
2020-04-29 12:28:51 +0200

    fixed problem where a 503 occurred and a doujinshi would be skipped

--- main.py
+++ main.py
@@ -1,60 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
 import shutil
+import time
 import re
 import os
 
 filepath = "target"
+
 if os.path.exists(filepath):
-    shutil.rmtree(filepath)
-    os.mkdir(filepath)
+    #shutil.rmtree(filepath)
+    #os.mkdir(filepath)
+    print("Directory Already Exists, appending current Dir")
 else:
     os.mkdir(filepath)
 
 def main():
-    for upload in range(0, 310717): # Data value for number of doujinshi to loop through
+    for upload in range(1, 310717): # Data value for number of doujinshi to loop through
         upload += 1
         out = mainPage(upload)
-        pagenum = subPage(out[0], out[1], out[2])
+        if out[1] != 0:
+            pagenum = subPage(out[0], out[1], out[2])
+        else:
+            time.sleep(3)
+            pass
 
 def mainPage(upload):
     url = f'https://nhentai.net/g/{upload}/1'
-    try:
-        resp = requests.get(url)
-    except OSError:
-        print("Error Occurred")
-        return
+    resp = requests.get(url)
 
     soup = BeautifulSoup(resp.content, "lxml")
     tit = soup.find('title')
     title = re.sub(r'-.*', "", tit.contents[0])
 
     info = soup.findAll('script')
-    totPage = re.split('[,.]', str(info[2]))
-    
-    totPage = re.search('num_pages\":(.*)}\',', str(totPage))
-    body = soup.find('section', attrs={"id": "image-container"}).findChild('img')
-    DojinDir = body['src'][:-5]
-    return DojinDir, title, totPage.group(1)
+    totPage = re.search('num_pages\":(.*)},', str(info))
 
-def subPage(DojinDir, title, totPage):
+    try:
+        body = soup.find('section', attrs={"id": "image-container"}).findChild('img')
+        DojinDir = body['src'][:-5]
+        return DojinDir, totPage.group(1), title
+    except:
+        print(title)
+        Success = 0
+        return title, Success
+
+def subPage(DojinDir, totPage, title):
     pagenum = 1
-    os.mkdir(f'{filepath}/{title}')
+
+    try:
+        os.mkdir(f'{filepath}/{title}')
+    except:
+        print("Doujinshi already downloaded")
+        pagenum += 1
+        return
     print(f'Number of pages: {totPage} - Name: {title}')
-    
-    while requests.get(f'{DojinDir}{pagenum}.jpg').status_code != 404:
-        try:
-            resp = requests.get(f'{DojinDir}{pagenum}.jpg')
-        except OSError:
-            print("Error Occurred")
-            return
-        
+
+    while requests.get(f'{DojinDir}{pagenum}.jpg').status_code == 200:
+        resp = requests.get(f'{DojinDir}{pagenum}.jpg')
         with open(f'{filepath}/{title}/{pagenum}.jpg', 'wb') as fileout:
             fileout.write(resp.content)
         pagenum += 1
-    if requests.get(f'{DojinDir}{pagenum}.jpg').status_code == 403:
-        print("Error 403, Access forbidden")
-        exit()
+        
+    if requests.get(f'{DojinDir}{pagenum}.jpg').status_code == 404 or 403:
+        print(f'Some generic error occurred. - g/{pagenum}/')
+        shutil.rmtree(f'{filepath}/{title}')
 
 if __name__ == '__main__':
     main()
